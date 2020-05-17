e7b49ebfb2b17f8c10d7b8f2dff626db74c82470
Williamnobuyuki <williamnobuyuki16@gmail.com>
2020-05-17 08:57:25 -0300

    ajuste

--- src/api/user/authService.js
+++ src/api/user/authService.js
@@ -1,6 +1,6 @@
 const _ = require('lodash')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 const User = require('./user')
 const env = require('../../.env')
 
@@ -16,13 +16,14 @@ const sendErrorsFromDB = (res, dbErrors) => {
 const login = (req, res, next) => {
     const email = req.body.email || ''
     const password = req.body.password || ''
+
     User.findOne({ email }, (err, user) => {
         if (err) {
             return sendErrorsFromDB(res, err)
         } else if (user && bcrypt.compareSync(password, user.password)) {
-            const token = jwt.sign(user.toJSON(), env.authSecret, {
-             expiresIn: "1 day"
-            });
+            const token = jwt.sign(user, env.authSecret, {
+                expiresIn: "1 day"
+            })
             const { name, email } = user
             res.json({ name, email, token })
         } else {
@@ -44,21 +45,25 @@ const signup = (req, res, next) => {
     const email = req.body.email || ''
     const password = req.body.password || ''
     const confirmPassword = req.body.confirm_password || ''
+
     if (!email.match(emailRegex)) {
-        return res.status(400).send({ errors: ['O e-mail informa está inválido'] })
+        return res.status(400).send({ errors: ['O e-mail informado está inválido'] })
     }
+
     if (!password.match(passwordRegex)) {
         return res.status(400).send({
             errors: [
-                "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$%) e tamanho entre 6-20."
+                "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$ %) e tamanho entre 6-20."
             ]
         })
     }
+
     const salt = bcrypt.genSaltSync()
     const passwordHash = bcrypt.hashSync(password, salt)
     if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
         return res.status(400).send({ errors: ['Senhas não conferem.'] })
     }
+
     User.findOne({ email }, (err, user) => {
         if (err) {
             return sendErrorsFromDB(res, err)
